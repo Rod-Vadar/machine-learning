3be5a736528ed20cc4ac03fb92a0e1902011e2fd
Rod-Vadar <105311434+Rod-Vadar@users.noreply.github.com>
2023-04-22 15:04:59 +0000

    Add private mouse move function

--- web/js/sketchPad.js
+++ web/js/sketchPad.js
@@ -10,18 +10,34 @@ class SketchPad {
         container.appendChild(this.canvas);
 
         this.ctx=this.canvas.getContext('2d');
+        this.path=[];
+        this.isDrawing=false;
 
         this.#addEventListeners();
     }
 
     #addEventListeners(){
         this.canvas.onmousedown=(event)=>{
-            const rect=this.canvas.getBoundingClientRect();
-            const mouse =[
-                event.clientX-rect.left,
-                event.clientY-rect.top
-            ];
-            console.log(mouse);
+            const mouse = this.#getMouse(event);
+            this.path=[mouse];
+            this.isDrawing=true;
         }
+        this.canvas.onmousemove=(event)=>{
+            if(this.isDrawing){
+                const mouse = this.#getMouse(event);
+                this.path.push(mouse);
+                console.log(this.path.length);
+            }
+        }
+        this.canvas.onmouseup=()=>{
+            this.isDrawing=false;
+        }
+    }
+    #getMouse=(event)=>{
+        const rect=this.canvas.getBoundingClientRect();
+        return [
+            Math.round(event.clientX-rect.left),
+            Math.round(event.clientY-rect.top)
+        ];
     }
 }
